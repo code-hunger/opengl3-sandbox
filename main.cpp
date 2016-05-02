5369fe8c04b212ace10a571e52ee5ed15b9a9d6a
Пешо Дебилов <beinghavingbreackfast@gmail.com>
2016-05-02 17:00:20 +0300

    Different vertices - different coloursclang-format -style=file

--- main.cpp
+++ main.cpp
@@ -11,18 +11,12 @@
 
 bool change_color = true;
 
-void error_callback(int error, const char *desc)
-{
-	printf("Error #%d: %s\n", error, desc);
-}
+void error_callback(int error, const char *desc) { printf("Error #%d: %s\n", error, desc); }
 
-static void key_callback(GLFWwindow *window, int key, int scancode, int action,
-                         int mods)
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
-	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
-		change_color = !change_color;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) change_color = !change_color;
 
 	printf("Recieved key event! Key: %d, scancode: %d, action: %d, "
 	       "mods: %d\n",
@@ -53,14 +47,17 @@ void loadShaders(GLuint *frag_shader, GLuint *vert_shader)
 {
 	const char *frag_shader_source = "#version 330 core\n\
                                       out vec4 color;\
-                                      uniform vec4 vertexColor;\
+                                      in vec3 vertexColor;\
                                       void main() {\
-                                          color = vertexColor;\
+                                          color = vec4(vertexColor, 1.0f);\
                                       } \0",
 	           *vert_shader_source = "#version 330 core\n\
                                       layout (location = 0) in vec2 position;\
+                                      layout (location = 1) in vec3 color;\
+                                      out vec3 vertexColor;\
                                       void main() { \
-                                          gl_Position = vec4(position.x, position.y, 0.0, 1.0);\
+                                          gl_Position = vec4(position.x, position.y, 0.0f, 1.0f);\
+                                          vertexColor = color;\
                                       } \0";
 	*frag_shader = loadAShader(frag_shader_source, GL_FRAGMENT_SHADER);
 	*vert_shader = loadAShader(vert_shader_source, GL_VERTEX_SHADER);
@@ -128,16 +125,17 @@ int main()
 	loadShaders(&frag_shader, &vert_shader);
 	GLuint shaderProgram = loadShaderProgram(frag_shader, vert_shader);
 
+	// clang-format off
 	GLfloat vertices[] = {
-	    -0.5f, -0.5f, // 0
-	    -0.3f, 0.5f,  // 1
-	    0.5f, 0.5f,   // 2
-	    0.5f, -0.5f,  // 3
-	    -0.75f, 0.f   // 4
+	    -0.5f,  -0.5f, .0f, .3f, .8f,
+	    -0.3f,   0.5f, .8f, 0.f, .5f,
+	     0.5f,   0.5f, 1.f, 1.f, 1.f,
+	     0.5f,  -0.5f, 1.f, 1.f, 0.f,
+	    -0.75f,  0.f,  .0f, .5f, .5f
 	};
+	// clang-format on
 
-	GLuint indices[] = {/* 0, 1, 2, 0, 2, 3, 0, 1, 4, */
-	                    0, 1, 3};
+	GLuint indices[] = {0, 1, 3, 4, 1, 2, 4, 1, 0};
 
 	GLuint VAO[2], VBO[2], EBO;
 	glGenVertexArrays(2, VAO);
@@ -151,12 +149,13 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-	             GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(GL_FLOAT),
-	                      (GLvoid *)0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid *)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid *)(2 * sizeof(GL_FLOAT)));
+    glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0); // unbind
@@ -165,26 +164,25 @@ int main()
 
 	glfwSetKeyCallback(window, key_callback);
 
-	GLint loc = glGetUniformLocation(shaderProgram, "vertexColor");
+	/* GLint loc = glGetUniformLocation(shaderProgram, "vertexColor"); */
 
 	while (!glfwWindowShouldClose(window)) {
 		int width, height;
-		double time = glfwGetTime();
+		/* double time = glfwGetTime(); */
 		glfwGetFramebufferSize(window, &width, &height);
 		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(0, 0.f, 0, 0);
 
-		if (change_color) {
-			float green = (float)(sin(time) / 2) + 0.5f;
-			glUniform4f(loc, (green / 2 + 0.5f), green, 1 - green, 1.f);
-		}
+		/* if (change_color) { */
+		/* 	float green = (float)(sin(time) / 2) + 0.5f; */
+		/* 	glUniform4f(loc, (green / 2 + 0.5f), green, 1 - green, 1.f); */
+		/* } */
 
 		glUseProgram(shaderProgram);
 
 		glBindVertexArray(VAO[0]);
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint),
-		               GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
 
 		glBindVertexArray(0); // unbind
 
