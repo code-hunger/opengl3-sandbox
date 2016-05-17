5a5ba60e15a0a8b725a8c4b8b039762ee387986e
Пешо Дебилов <beinghavingbreackfast@gmail.com>
2016-05-17 14:28:12 +0300

    Interesting effect (bug) when using lookAt() Use "using glm::vec3" and mat4, too.

--- Window.cpp
+++ Window.cpp
@@ -1,5 +1,8 @@
 #include "Window.h"
 
+using glm::vec3;
+using glm::mat4;
+
 Window::Window(GLFWwindow *window) : window(window)
 {
 	printf("Window constructed!\n");
@@ -40,20 +43,23 @@ void Window::render(const double deltaTime, const ShaderPrograms &programs,
 	    static_cast<float>(2 + cos(glfwGetTime()) * 2) * 10 + 2;
 	scale_factor = static_cast<float>(glfwGetTime() * 30);
 
-	glm::mat4 model;
-	model = glm::rotate(model, scale_factor, glm::vec3(0.8f, 1.0f, .0f));
+	mat4 model;
+	model = glm::rotate(model, scale_factor, vec3(0.8f, 1.0f, .0f));
 
-	glm::mat4 view;
+	mat4 view;
 
 	int BOX_COUNT = 20, SPACE_BETWEEN = 10;
 
 	float otmestvane = static_cast<float>(-50 + 25 * glfwGetTime());
 	while (otmestvane > static_cast<float>(BOX_COUNT * SPACE_BETWEEN / 2))
 		otmestvane -= static_cast<float>(BOX_COUNT * SPACE_BETWEEN / 2);
-	view = glm::translate(
-	    view, glm::vec3(-2.5f + deltaX * 8, -deltaY * 8, otmestvane));
 
-	glm::mat4 proj;
+	/* view = glm::translate( */
+	/*     view, vec3(-2.5f + deltaX * 8, -deltaY * 8, otmestvane)); */
+
+    view = glm::lookAt(vec3(-2.5f + deltaX * 8, -deltaY * 8, otmestvane), vec3(0,0,0), vec3(0, 1, 0));
+
+	mat4 proj;
 	proj = glm::perspective(45.f, 1.0f, 0.1f, 100.f);
 
 	glUniformMatrix4fv(mvpLoc, 1, GL_FALSE,
@@ -61,24 +67,24 @@ void Window::render(const double deltaTime, const ShaderPrograms &programs,
 
 	programs[0]->use();
 
-    glm::mat4 me_view, me_model;
-    me_view = glm::translate(me_view, glm::vec3(0, -.6, -4));
-    me_model = glm::scale(me_model, glm::vec3(.5, .5, .5));
-    me_model = glm::rotate(me_model, deltaX / 2, glm::vec3(0, 0, -10));
+    mat4 me_view, me_model;
+    me_view = glm::translate(me_view, vec3(0, -.6, -4));
+    me_model = glm::scale(me_model, vec3(.5, .5, .5));
+    me_model = glm::rotate(me_model, deltaX / 2, vec3(0, 0, -10));
     glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(proj * me_view * me_model));
     vertArrays[0]->draw(GL_TRIANGLES, 0);
 
 	for (int i = 0; i < BOX_COUNT; ++i) {
-		view = glm::translate(view, glm::vec3(0, 0, -SPACE_BETWEEN));
+		view = glm::translate(view, vec3(0, 0, -SPACE_BETWEEN));
 		glUniformMatrix4fv(mvpLoc, 1, GL_FALSE,
 		                   glm::value_ptr(proj * view * model));
 		vertArrays[0]->draw(GL_TRIANGLES, 0);
 	}
-	view = glm::translate(view, glm::vec3(5, 0, SPACE_BETWEEN * BOX_COUNT));
-	model = glm::rotate(model, -2 * scale_factor, glm::vec3(0.8f, 1.f, 0));
+	view = glm::translate(view, vec3(5, 0, SPACE_BETWEEN * BOX_COUNT));
+	model = glm::rotate(model, -2 * scale_factor, vec3(0.8f, 1.f, 0));
 
 	for (int i = 0; i < BOX_COUNT; ++i) {
-		view = glm::translate(view, glm::vec3(0, 0, -SPACE_BETWEEN));
+		view = glm::translate(view, vec3(0, 0, -SPACE_BETWEEN));
 		glUniformMatrix4fv(mvpLoc, 1, GL_FALSE,
 		                   glm::value_ptr(proj * view * model));
 		vertArrays[0]->draw(GL_TRIANGLES, 0);
