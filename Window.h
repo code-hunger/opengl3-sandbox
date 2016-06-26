e8a32c6aeffe730153a8f293f908996d6d2905e5
Пешо Дебилов <beinghavingbreackfast@gmail.com>
2016-06-26 22:37:04 +0300

    move glm to the /lib directory

--- Window.h
+++ Window.h
@@ -11,9 +11,9 @@
 #pragma GCC diagnostic ignored "-Weffc++"
 #pragma GCC diagnostic ignored "-Wpedantic"
 #pragma GCC diagnostic ignored "-Wold-style-cast"
-#include "glm/glm/glm.hpp"
-#include "glm/glm/gtc/matrix_transform.hpp"
-#include "glm/glm/gtc/type_ptr.hpp"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 #pragma GCC diagnostic pop
 
 class ShaderProgram; 
