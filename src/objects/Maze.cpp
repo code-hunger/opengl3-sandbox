2daa1fe4e4611011a67bf1137a9e86f75d0bba05
Пешо Дебилов <beinghavingbreackfast@gmail.com>
2016-07-07 19:06:04 +0300

    Draw parallel lines to the ones in maze1.txt

--- src/objects/Maze.cpp
+++ src/objects/Maze.cpp
@@ -1,18 +1,41 @@
 #include "Maze.h"
-#include <assert.h>
+#include <cassert>
+#include <cmath>
 #define MAX_LINES 10
+#define PI 3.141592653589793
 
 inline VertexArray generateVertArr(Lines lines)
 {
+	assert(lines.size() <= MAX_LINES);
 	puts("Generating vertex array for the maze\n");
 
-    assert(lines.size() <= MAX_LINES);
-	float points[MAX_LINES * 4];
+	float points[MAX_LINES * 4 * 3];
 
 	int i = 0;
 	for (const auto &line : lines) {
+		double line_angle = atan((line.b.y - line.a.y) / (line.b.x - line.a.x));
+		float angle_sin = static_cast<float>(sin(line_angle)),
+		      angle_cos = static_cast<float>(cos(line_angle)),
+		      deltaXA = line.weight_a * angle_sin,
+		      deltaXB = line.weight_b * angle_sin,
+		      deltaYA = line.weight_a * angle_cos,
+		      deltaYB = line.weight_b * angle_cos;
+
+		points[i++] = line.a.x + deltaXA;
+		points[i++] = line.a.y - deltaYA;
+
+		points[i++] = line.b.x + deltaXB;
+		points[i++] = line.b.y - deltaYB;
+
+		points[i++] = line.b.x - deltaXB;
+		points[i++] = line.b.y + deltaYB;
+
+		points[i++] = line.a.x - deltaXA;
+		points[i++] = line.a.y + deltaYA;
+
 		points[i++] = line.a.x;
 		points[i++] = line.a.y;
+
 		points[i++] = line.b.x;
 		points[i++] = line.b.y;
 	}
