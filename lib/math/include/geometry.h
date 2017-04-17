8b4578870a8793827c9ec947438880a757f2fd9e
Code Hunger <beinghavingbreackfast@gmail.com>
2017-04-17 14:24:17 +0300

    Extract Color::next definition out of types.h

--- lib/math/include/geometry.h
+++ lib/math/include/geometry.h
@@ -176,6 +176,20 @@ inline ColorSegment2 toWhiteSegment2(const WideRoad2& way)
 	return toColorSegment(way, {0.7f, 0.7f, 0.7f, "white"});
 }
 
+const Color& Color::next()
+{
+	static const std::vector<Color> colors = {
+	    {1, 1, 0.1f, "L yellow"},    {0, 0.3f, 1.f, "D blue"},
+	    {0.5f, 0, 0.8f, "violet"},   {1, 0.5f, 0, "orange"},
+	    {.5f, .5f, .5f, "silver"},   {1.f, 0, 0, "red"},
+	    {1.f, 0.5f, 0.5f, "pink"},   {0, .7f, 1.f, "L blue"},
+	    {0.7f, 0.2f, 0.3f, "brown"}, {0.2f, 1, 0.2f, "L green"},
+	    {0, 0.5f, 0, "D green"}};
+	static unsigned long color_count = colors.size(), color = 0;
+
+	return colors[(color++) % color_count];
+}
+
 // struct Hash
 //{
 // size_t operator()(const WideRoad2& way) const
--- lib/math/include/types.h
+++ lib/math/include/types.h
@@ -14,19 +14,7 @@ struct Color
 	char const* const name;
 	operator const char*() const { return name; }
 
-	static const Color& next()
-	{
-		static const std::vector<Color> colors = {
-		    {1, 1, 0.1f, "L yellow"},    {0, 0.3f, 1.f, "D blue"},
-		    {0.5f, 0, 0.8f, "violet"},   {1, 0.5f, 0, "orange"},
-		    {.5f, .5f, .5f, "silver"},   {1.f, 0, 0, "red"},
-		    {1.f, 0.5f, 0.5f, "pink"},   {0, .7f, 1.f, "L blue"},
-		    {0.7f, 0.2f, 0.3f, "brown"}, {0.2f, 1, 0.2f, "L green"},
-		    {0, 0.5f, 0, "D green"}};
-		static unsigned long color_count = colors.size(), color = 0;
-
-		return colors[(color++) % color_count];
-	}
+	static const Color& next();
 };
 
 struct Point2
