af006e35d371c1f409dd31873ce00cd555e966f0
Code Hunger <beinghavingbreackfast@gmail.com>
2017-04-29 11:49:35 +0300

    Take the implementation out of GlfwWrapper header

--- lib/graphics/GlfwWrapper.cpp
+++ lib/graphics/GlfwWrapper.cpp
@@ -1,3 +1,40 @@
 #include "GlfwWrapper.h"
 
-// Turns out there's nothing to do here...
+#include "logger/include/logger.h"
+#include <GLFW/glfw3.h>
+
+static void setWindowHints()
+{
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#ifdef ANTI_ALIASING
+	glfwWindowHint(GLFW_SAMPLES, 4);
+#endif
+}
+
+static void errorCallback(int code, const char* desc)
+{
+	ERR("GLFW error #%d: %s", code, desc);
+	throw "Error callback called! Look at stderr.";
+}
+
+GlfwWrapper::GlfwWrapper()
+{
+	if (exists) throw "No second glfw wrapper allowed";
+	if (!glfwInit()) throw "Glfw init fail!";
+
+	glfwSetErrorCallback(errorCallback);
+	setWindowHints();
+	exists = true;
+
+	LOG << "GlfwWrapper created";
+}
+
+GlfwWrapper::~GlfwWrapper() {
+	exists = false;
+	glfwTerminate();
+	LOG("GlfwWrapper exited");
+}
--- lib/graphics/include/GlfwWrapper.h
+++ lib/graphics/include/GlfwWrapper.h
@@ -2,43 +2,13 @@
 #define GLFWWRAPPER_H_2OBZJPPD
 
 #include "Window.h"
-#include "logger/include/logger.h"
-#include <GLFW/glfw3.h>
-
-static void setWindowHints()
-{
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#ifdef ANTI_ALIASING
-	glfwWindowHint(GLFW_SAMPLES, 4);
-#endif
-}
-
-static void errorCallback(int code, const char* desc)
-{
-	ERR("GLFW error #%d: %s", code, desc);
-	throw "Error callback called! Look at stderr.";
-}
 
 class GlfwWrapper
 {
 	static bool exists;
 
 public:
-	GlfwWrapper()
-	{
-		if (exists) throw "No second glfw wrapper allowed";
-		if (!glfwInit()) throw "Glfw init fail!";
-
-		glfwSetErrorCallback(errorCallback);
-		setWindowHints();
-		exists = true;
-
-		LOG << "GlfwWrapper created";
-	}
+	GlfwWrapper();
 
 	GlfwWrapper(GlfwWrapper&&) = default;
 
@@ -47,12 +17,7 @@ public:
 
 	Window acquireWindow() & { return {}; }
 
-	~GlfwWrapper()
-	{
-		exists = false;
-		glfwTerminate();
-		LOG("GlfwWrapper exited");
-	}
+	~GlfwWrapper();
 };
 
 #endif /* end of include guard: GLFWWRAPPER_H_2OBZJPPD */
