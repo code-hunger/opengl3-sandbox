7aae0bdea8a169507dd570e08f080042143c4c0a
Code Hunger <beinghavingbreackfast@gmail.com>
2017-05-21 23:18:10 +0300

    More readable Ship vertices; make squares smaller

--- src/include/ShipsCollection.h
+++ src/include/ShipsCollection.h
@@ -22,8 +22,11 @@ private:
 	std::vector<Ship> ships{};
 
 	VertexArray vertexArray{{
-	    0, 0, 0, 1,  1, 1, 0, 10, 0, 1, 1, 1, 10, 10, 0,
-	    1, 1, 1, 10, 0, 0, 1, 1,  1, 0, 0, 0, 1,  1,  1,
+	    -2, -2, 0, 1, 1, 1, // first point
+	    -2, 2,  0, 1, 1, 1, // second point
+	    2,  2,  0, 1, 1, 1, // third point
+	    2,  -2, 0, 1, 1, 1, // 4th point
+	    -2, -2, 0, 1, 1, 1, // 1st point again
 	}};
 };
 
