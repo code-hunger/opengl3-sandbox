494865c2079d821b05a10e071948f06b3e9f1c00
Code Hunger <beinghavingbreackfast@gmail.com>
2017-05-21 23:18:43 +0300

    Faster rotation for easier testing

--- src/objects/Ship.cpp
+++ src/objects/Ship.cpp
@@ -10,10 +10,10 @@ Ship::Ship(math::Point2 position, double direction)
 void Ship::update(const State& state, double deltaTime)
 {
 	if (state.keys[GLFW_KEY_LEFT]) {
-		direction += deltaTime * 1.6;
+		direction += deltaTime * 3.6;
 	}
 	if (state.keys[GLFW_KEY_RIGHT]) {
-		direction -= deltaTime * 1.6;
+		direction -= deltaTime * 3.6;
 	}
 	if (state.keys[GLFW_KEY_SPACE]) {
 		position.x += cos(direction) * 20 * deltaTime;
