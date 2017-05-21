1fba64a49709e8023993e7ba8f3d6abf2a042ec9
Code Hunger <beinghavingbreackfast@gmail.com>
2017-05-21 23:10:24 +0300

    Add simple keyboard controls to Ships

--- src/objects/Ship.cpp
+++ src/objects/Ship.cpp
@@ -1,8 +1,22 @@
 #include "Ship.h"
 
+#include <GLFW/glfw3.h>
+
 Ship::Ship(math::Point2 position, double direction)
     : position(position), direction(direction)
 {
 }
 
-void Ship::update(const State&, double) {}
+void Ship::update(const State& state, double deltaTime)
+{
+	if (state.keys[GLFW_KEY_LEFT]) {
+		direction += deltaTime * 1.6;
+	}
+	if (state.keys[GLFW_KEY_RIGHT]) {
+		direction -= deltaTime * 1.6;
+	}
+	if (state.keys[GLFW_KEY_SPACE]) {
+		position.x += cos(direction) * 20 * deltaTime;
+		position.y += sin(direction) * 20 * deltaTime;
+	}
+}
