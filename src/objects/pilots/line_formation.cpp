9d1c7bd25c49689bda7095717c179b29a4e62aed
Code Hunger <beinghavingbreackfast@gmail.com>
2017-07-31 01:03:08 +0300

    Invert and simplify checks and remove unused code

--- src/objects/pilots/line_formation.cpp
+++ src/objects/pilots/line_formation.cpp
@@ -9,48 +9,38 @@ void follower::operator()(Ship& ship)
 {
 	float currentDistance = static_cast<float>(
 	    sqrt(ship.getPosition().distance2(leader->getPosition())));
-	if (currentDistance > distance) {
-		ship.startMoving(1);
 
-		const float cosTarget = (leader->x - ship.x) / currentDistance,
-		            sinTarget = (leader->y - ship.y) / currentDistance;
-
-		float currentDirection = ship.getDirection(),
-		      targetDirection = asinf(sinTarget);
-
-		if (cosTarget < 0) {
-			targetDirection = static_cast<float>(PI) - targetDirection;
-		} else if (sinTarget < 0) {
-			targetDirection += static_cast<float>(2 * PI);
-		}
-
-		const float cosCurrent = cosf(currentDirection),
-		            sinCurrent = sinf(currentDirection);
-
-		float diff = 0;
-		if (sinCurrent > 0) {
-			if (sinTarget > 0) {
-				diff = -cosTarget + cosCurrent;
-			} else {
-				diff = -cosTarget - cosCurrent;
-			}
-		} else {
-			if (sinTarget > 0) {
-				diff = cosTarget + cosCurrent;
-			} else {
-				diff = cosTarget - cosCurrent;
-			}
-		}
-
-		if (diff > 1e-5)
-			ship.rotate(LEFT);
-		else if (diff < -1e-5)
-			ship.rotate(RIGHT);
-		else
-			ship.rotate(NONE);
-	} else {
+	if (currentDistance < distance) {
 		ship.stopMoving();
 		ship.rotate(NONE);
+		return;
 	}
+
+	ship.startMoving(1);
+
+	const float cosTarget = (leader->x - ship.x) / currentDistance,
+	            sinTarget = (leader->y - ship.y) / currentDistance,
+
+	            currentDirection = ship.getDirection(),
+	            cosCurrent = cosf(currentDirection),
+	            sinCurrent = sinf(currentDirection);
+
+	float diff = 0;
+	if (sinCurrent > 0) {
+		if (sinTarget > 0) {
+			diff = -cosTarget + cosCurrent;
+		} else {
+			diff = -cosTarget - cosCurrent;
+		}
+	} else {
+		if (sinTarget > 0) {
+			diff = cosTarget + cosCurrent;
+		} else {
+			diff = cosTarget - cosCurrent;
+		}
+	}
+
+	// Negative diff means turn left, positive means turn right
+	ship.rotate(diff > 1e-5 ? LEFT : diff < -1e-5 ? RIGHT : NONE);
 }
 }
