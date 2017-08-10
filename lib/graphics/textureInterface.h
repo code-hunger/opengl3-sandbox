c0788415781fa8cc6d4e7d86c276bf61e9a682a0
Viktorsmg <Viktor.smg@gmail.com>
2017-08-11 02:28:48 +0300

    textureIntf initial implementation
    Header has most of what will be needed laid out, but the interface itself, even in its most basic form, is resolutely UNFINISHED.

---
+++ lib/graphics/textureInterface.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#ifndef TEXTUREINTERFACE_H
+
+#define TEXTUREINTERFACE_H
+
+#include<string>
+
+typedef unsigned int uint;
+
+class textureInterface
+{
+public:
+	float *data;
+	uint size;
+	std::string fmt,fileName;
+
+	textureInterface();
+	textureInterface(std::string &_fileName);
+	textureInterface(float _data[], uint _size, std::string _fmt);
+	textureInterface(textureInterface &_texIntf);
+
+	//Load and copy methods return whether they were successful or not.
+	bool loadFile(std::string &_fileName);
+	bool copyData(float _data[], uint _size, std::string _fmt);
+
+	~textureInterface();
+private:
+
+	//All load methods return whether they were successful or not. 
+	bool loadPNG(); bool savePNG();
+	bool loadTGA(); bool saveTGA();
+	bool loadBMP(); bool saveBMP();
+	bool loadJPEG(); bool saveJPEG();//Did you know? Early windows supported 3-letter extensions only, so no jpeg for them; but macintosh supported more, so the original and more correct term would be jpeg.
+	// ^Non-float image formats
+	//
+	// \/ Float image formats
+	bool loadEXR(); bool saveEXR();
+	bool loadHDR(); bool saveHDR();
+	bool loadTIFF(); bool saveTIFF();
+};
+
+#endif
