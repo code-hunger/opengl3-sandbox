ba36f45c60336a419d5a06cb13f81d6e3cc7250c
Viktorsmg <Viktor.smg@gmail.com>
2017-08-13 00:19:30 +0300

    Add image types and short/int types
    Switch to mime later on if feeling like it. Bonus, string representation of imageType for easy comparing in order to find the proper imageType

--- lib/graphics/TextureLoader.h
+++ lib/graphics/TextureLoader.h
@@ -3,8 +3,15 @@
 
 #define TEXTURELOADER_H
 
+typedef unsigned int uint;
+typedef unsigned short ushort;
+
+enum imageType{UNKNOWN, PNG, TGA, BMP, JPEG, DDS, EXR, HDR, TIFF};
+const ushort IMAGETYPE_COUNT = 9;
+const std::string imageType_str[IMAGETYPE_COUNT] = {"UNKNOWN", "PNG", "TGA", "BMP", "JPEG", "DDS", "EXR", "HDR", "TIFF"}//When we have a mime type table handy, delete these
+
 class TextureLoader{
-	
+
 };
 
 #endif
