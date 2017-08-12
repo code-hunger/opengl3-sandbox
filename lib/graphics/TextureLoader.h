6bfe608d6a057cc2af32e7a40f99adf290792c54
Viktorsmg <Viktor.smg@gmail.com>
2017-08-13 00:25:17 +0300

    Add basic template for image type loading and logging
    The template saves using loads of saveSTH() and loadSTH() methods...

--- lib/graphics/TextureLoader.h
+++ lib/graphics/TextureLoader.h
@@ -3,6 +3,9 @@
 
 #define TEXTURELOADER_H
 
+#include "logger/logger.h"
+#include <string>
+
 typedef unsigned int uint;
 typedef unsigned short ushort;
 
@@ -11,6 +14,17 @@ const ushort IMAGETYPE_COUNT = 9;
 const std::string imageType_str[IMAGETYPE_COUNT] = {"UNKNOWN", "PNG", "TGA", "BMP", "JPEG", "DDS", "EXR", "HDR", "TIFF"}//When we have a mime type table handy, delete these
 
 class TextureLoader{
+
+	//Both of these should return whether they were successful or not
+	template<imageType iType> bool loadFile(const std::string& fileName){//TODO: Think of better name for iType
+		LOG("Loading for image type %i(%s) not implemented!", iType, imageType_str[iType].data());
+		return false;
+	}
+
+	template<imageType iType> bool saveFile(const std::string& fileName) {//TODO: Think of better name for iType
+		LOG("Saving for image type %i(%s) not implemented!", iType, imageType_str[iType].data());
+		return false;
+	}
 
 };
 
