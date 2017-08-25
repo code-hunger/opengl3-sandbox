e2302685da44f74e1c1cacb7c34c3aefef72bf12
Code Hunger <beinghavingbreackfast@gmail.com>
2017-08-25 15:49:39 +0300

    Move ConfigFactory ctor to source
    Removes TCLAP as a direct dependency of int main()

--- src/include/ConfigFactory.h
+++ src/include/ConfigFactory.h
@@ -3,7 +3,6 @@
 
 #include "ScreenManager.h"
 #include "graphics/Window.h"
-#include "tclap/CmdLine.h"
 
 struct ConfigFactory
 {
@@ -11,38 +10,19 @@ struct ConfigFactory
 	bool no_join_lines, print_fps;
 
 public:
-	ConfigFactory(int argc, char** argv)
-	{
-		using namespace TCLAP;
-
-		CmdLine cmd("ScreenManager title", ' ', "0.1");
-		UnlabeledValueArg<ushort> maze_id("maze_id", "Maze id to load", false,
-		                                  1, "number", cmd);
-		ValueArg<ushort> max_lines("m", "max_lines", "Max ways to be added",
-		                           false, 0, "count", cmd);
-		SwitchArg no_join_lines("n", "nojoin", "Don't join intersecting lines",
-		                        cmd, true),
-		    print_fps("f", "print-fps", "prints fps", cmd);
-
-		cmd.parse(argc, argv);
-
-		this->maze_id = maze_id.getValue();
-		this->no_join_lines = no_join_lines.getValue();
-		this->max_lines = max_lines.getValue();
-		this->print_fps = print_fps.getValue();
-	}
+	ConfigFactory(int argc, char** argv);
 
 	template <typename T> T produce() { return {}; };
 
 	template <typename T> void process(T&) {}
 };
 
-template <> ScreenManager ConfigFactory::produce()
+template <> inline ScreenManager ConfigFactory::produce()
 {
 	return {maze_id, no_join_lines, max_lines};
 }
 
-template <> void ConfigFactory::process(Window& window)
+template <> inline void ConfigFactory::process(Window& window)
 {
 	window.setPrintFps(print_fps);
 }
---
+++ src/objects/ConfigFactory.cpp
@@ -0,0 +1,23 @@
+#include "ConfigFactory.h"
+#include "tclap/CmdLine.h"
+
+ConfigFactory::ConfigFactory(int argc, char** argv)
+{
+	using namespace TCLAP;
+
+	CmdLine cmd("ScreenManager title", ' ', "0.1");
+	UnlabeledValueArg<ushort> maze_id("maze_id", "Maze id to load", false, 1,
+	                                  "number", cmd);
+	ValueArg<ushort> max_lines("m", "max_lines", "Max ways to be added", false,
+	                           0, "count", cmd);
+	SwitchArg no_join_lines("n", "nojoin", "Don't join intersecting lines", cmd,
+	                        true),
+	    print_fps("f", "print-fps", "prints fps", cmd);
+
+	cmd.parse(argc, argv);
+
+	this->maze_id = maze_id.getValue();
+	this->no_join_lines = no_join_lines.getValue();
+	this->max_lines = max_lines.getValue();
+	this->print_fps = print_fps.getValue();
+}
