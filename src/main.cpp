8813dc8d69adc6c8362308f52ef119e6ec8f6cb0
Code Hunger <beinghavingbreackfast@gmail.com>
2017-08-28 15:55:12 +0300

    Produce glfwWrapper from ConfigFactory
    To ensure ConfigFactory is successfully instantiated when creating
    windows. E.g. if wrong config parameters are passed, we shouldn't open
    any windows as ConfigFactory will throw/exit anyway.

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,10 @@ template <> void ConfigFactory::process(Window& window) const;
 int main(int argc, const char** argv)
 {
 	try {
-		GlfwWrapper wrapper;
-		auto window = wrapper.acquireWindow<Window>();
-
 		ConfigFactory factory{argc, argv};
+
+		auto wrapper = factory.produce<GlfwWrapper>();
+		auto window = wrapper.acquireWindow<Window>();
 		factory.process(window);
 	} catch (int e) {
 		ERR << "Something bad happened. Code " << e;
